88201d57a39370ed44d328d0d52724fd62096c1b
Jumpaku <itzimeru@gmail.com>
2019-04-13 10:02:23 +0900

    added web resources

--- src/main/kotlin/jumpaku/othello/api/Main.kt
+++ src/main/kotlin/jumpaku/othello/api/Main.kt
@@ -1,6 +1,7 @@
 package jumpaku.othello.api
 
 import io.ktor.application.install
+import io.ktor.features.CORS
 import io.ktor.features.CallLogging
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -9,6 +10,7 @@ import io.ktor.server.netty.Netty
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, 8080) {
         install(CallLogging)
+        install(CORS)
         modules()
     }
     server.start(wait = true)
---
+++ src/main/web/src/scripts/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import './App.css'
+
+export default class App extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+        <h1>Hello World from TypeScript! AAA</h1>
+      </div>
+    )
+  }
+}
